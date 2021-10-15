65f9378fc29ebbf6cf0d1b69c187fa67180fb2e4
magusin <fanatsy4@gmail.com>
2021-10-15 22:56:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { Header } from './components'
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+    state = {
+      post: [],  
+    }
+    
+ 
+
+async componentDidMount(){
+    const url = 'https://api.magicthegathering.io/v1/cards/';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({post: data.cards})
+    
+        
+        
+}
+
+
+  render () {
+  // const magic = this.state.post.map()
+    return (
+     
+      <div className="App ">
+         <Header />
+         <div>
+         {this.state.post.map((item) =>
+       <div> <h2>{item.name}</h2> 
+       <img src={item.imageUrl}></img>  </div>
+      )} 
+      </div>
+      </div>
+    );
+   
+  }
+
 }
 
 export default App;
