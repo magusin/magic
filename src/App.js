d148128b0f8023272e20ce888b5605d7425fcb7e
magusin <fanatsy4@gmail.com>
2021-10-17 13:41:19 +0200

    map carte langage

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import { Header } from "./components";
-import React, { Component, useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import React, {
+  Component, useState, useEffect, useDebugValue,
+} from 'react';
+import ReactDOM from 'react-dom';
+import { Header } from './components';
 
-import "./App.css";
+import './App.css';
 
 const App = () => {
-   // useState
+  // useState
   const [cards, setCards] = useState([]);
-// apiurl
-  const APIurl = "https://api.magicthegathering.io/v1/cards";
-  //useEffect
-   
-      useEffect( () =>{
-       return fetch(APIurl)
-         .then(res => res.json())
-         .then(data => setCards(data.cards));
-      
-      },[]);
+  const [translatedCards, setTranslatedCards] = useState([]);
+  // apiurl
+  const APIurl = 'https://api.magicthegathering.io/v1/cards';
+  // useEffect
 
-    return (
-      <div className="App ">
-        <Header />
-        <div>
-          {cards.map((item) => (
-            <div>
-              <h2>{item.name}</h2>
-              <img src={item.imageUrl}></img>
-            </div>
-          ))}
-         
-        </div>
+  useEffect(() => fetch(APIurl).then((res) => res.json())
+    .then((data) => {
+      const tcs = data.cards.map((c) => {
+        const tc = JSON.parse(JSON.stringify(c));
+        if (c.foreignNames) {
+          tc.name = c.foreignNames[1].name;
+          tc.imageUrl = c.foreignNames[1].imageUrl;
+        }
+        return tc;
+      });
+      setCards(data.cards);
+      setTranslatedCards(tcs);
+    }),
+  []);
+
+  return (
+    <div className="App ">
+      <Header />
+      <div>
+        {(translatedCards || []).map((item) => (
+          <div>
+            <h2>{item.name}</h2>
+            <img alt={item.name} src={item.imageUrl} />
+          </div>
+        ))}
+
       </div>
-    );
-}
-
-
-
+    </div>
+  );
+};
 
 export default App;
