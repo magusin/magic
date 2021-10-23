70e6d53d1f98649a858e2d523f40aaf0e666a46a
magusin <fanatsy4@gmail.com>
2021-10-23 03:55:52 +0200

    fix setextension value

--- src/components/extension/Extension.js
+++ src/components/extension/Extension.js
@@ -1,17 +1,30 @@
 const Extension = ({ sets, setExtension }) => {
-  console.log(sets);
+  // console.log(sets);
+
+  function catch_value() {
+    var target = document.getElementById("choose_sets").value;
+    var opts = document.getElementById("sets").childNodes;
+    for (var i = 0; i < opts.length; i++) {
+      if (opts[i].value === target) {
+        // An item was selected from the list!
+        // yourCallbackHere()
+        return opts[i].value;
+      }
+    }
+  }
+
   return (
     <div className="dropdown">
       <label htmlFor="choose_sets">Recherche extension :</label>
-      <input list="sets" type="text" id="choose_sets"/>
+      <input
+        list="sets"
+        type="text"
+        id="choose_sets"
+        onInput={() => setExtension(catch_value())}
+      />
       <datalist id="sets">
         {sets.map((item) => (
-          <option
-            onClick={() => setExtension(item.name)}
-            key={item.name}
-            value={item.name}
-          />
-          
+          <option key={item.name} value={item.name} />
         ))}
       </datalist>
     </div>
